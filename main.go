2100edddf3328836f8626be2f9c3870b1aac0418
Saransh Ambarte <saru10081999@gmail.com>
2024-07-06 00:26:40 +0530

    Add scan results page and improve logic

--- main.go
+++ main.go
@@ -1,17 +1,20 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
 	"net/http"
 	"os/exec"
 )
 
 var tpl *template.Template
+var outputString string
 
 func main() {
 	tpl, _ = tpl.ParseGlob("webpages/*.html")
 	http.HandleFunc("/", homeHandler)
 	http.HandleFunc("/imagescan", imageScanHandler)
+	http.HandleFunc("/scanresult", scanResultHandler)
 	http.ListenAndServe(":8888", nil)
 }
 
@@ -22,19 +25,23 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func imageScanHandler(w http.ResponseWriter, r *http.Request) {
-	imageName := r.FormValue("imageName")
-	println(imageName)
-	tpl.ExecuteTemplate(w, "loadpage.html", nil)
-	cmd := exec.Command("trivy image -f json " + imageName)
-	output, err := cmd.Output()
-	if err != nil {
-		println("Error:", err)
-	}
-	println(output)
-	printImageScanResult(w, output)
+func scanResultHandler(w http.ResponseWriter, r *http.Request) {
+	tpl.ExecuteTemplate(w, "scanresults.html", outputString)
 }
 
-func printImageScanResult(w http.ResponseWriter, output []byte) {
-	tpl.ExecuteTemplate(w, "scaresults.html", output)
+func imageScanHandler(w http.ResponseWriter, r *http.Request) {
+	imageName := r.FormValue("imageName")
+	tpl.ExecuteTemplate(w, "loadpage.html", nil)
+	cmd := exec.Command("trivy", "image", "-f", "json", imageName)
+	output, err := cmd.Output()
+	if err != nil {
+		fmt.Println("Error:", err)
+		if err.Error() == "exit status 1" {
+			outputString = "Please make sure that the image exists"
+		} else {
+			outputString = err.Error()
+		}
+	} else {
+		outputString = string(output)
+	}
 }
